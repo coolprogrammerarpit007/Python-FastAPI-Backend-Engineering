78942f436d99fa5132b6acef5307f904fd6768a8
Arpit <arpit.mishra@microprixs.in>
2025-12-17 22:45:22 +0530

    List,sets and tuples data structures in python assignment

---
+++ datastructures.py
@@ -0,0 +1,9 @@
+zoo_animals = ['lion','tiger','elephant','bear','monkey']
+# print(zoo_animals)
+
+removed_animal = zoo_animals.pop(3)
+# print(zoo_animals)
+
+zoo_animals.append("Zebra")
+# print(zoo_animals)
+# print(zoo_animals[0:3])
